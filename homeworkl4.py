739de358a6cd8aeb06729a4a23b07ebac0fc0b58
Linkus2Zanzibar <tkhan121@gmail.com>
2024-11-10 09:53:35 -0500

    this is the homework called recurrence relations

---
+++ homeworkl4.py
@@ -0,0 +1,12 @@
+def myfuntion1(n):
+    if(n>0):
+        return
+    for i in range (0,n+1):
+        print("codingal")
+    myfuntion1(n/2)
+    myfuntion1(n/3)
+def myfuntion(n):
+    if(n<=1):
+        return
+    print("codigal")
+    myfuntion2(n-1)
